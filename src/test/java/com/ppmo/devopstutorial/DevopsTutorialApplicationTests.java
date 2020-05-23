1c21d26b658c0289c547a604f8ba12f3e635a282
Paul-Philip <paulphilip.mosulet@gmail.com>
2020-05-23 16:27:38 +0200

    Added spring project

---
+++ src/test/java/com/ppmo/devopstutorial/DevopsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppmo.devopstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
