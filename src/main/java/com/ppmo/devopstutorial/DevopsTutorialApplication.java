51b7a34aae69d106ff8304e37302c38dd31bd976
Paul-Philip <paulphilip.mosulet@gmail.com>
2020-05-31 17:52:43 +0200

    Added some errors to SonarQube

--- src/main/java/com/ppmo/devopstutorial/DevopsTutorialApplication.java
+++ src/main/java/com/ppmo/devopstutorial/DevopsTutorialApplication.java
@@ -1,5 +1,7 @@
 package com.ppmo.devopstutorial;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsTutorialApplication {
 
 	public static void main(String[] args) {
+		AtomicInteger int1 = new AtomicInteger(1);
+		AtomicInteger int2 = new AtomicInteger(2);
+
+		if(int1.equals(int2)) {
+			System.out.println("Get outta here, they are not eqaul");
+		} else {
+			System.out.println("1 and 2 are not equal no...");
+		}
+
 		SpringApplication.run(DevopsTutorialApplication.class, args);
 	}
 
--- src/test/java/com/ppmo/devopstutorial/DevopsTutorialApplicationTests.java
+++ src/test/java/com/ppmo/devopstutorial/DevopsTutorialApplicationTests.java
@@ -3,11 +3,14 @@ package com.ppmo.devopstutorial;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class DevopsTutorialApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
